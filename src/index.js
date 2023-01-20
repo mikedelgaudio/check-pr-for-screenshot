309338946bdece5acdbbbbd2c2131ec6d57afabb
Mike DelGaudio <delgaudiomike@gmail.com>
2023-01-20 13:37:47 -0500

    add console log

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 import * as core from "@actions/core";
-const { context } = require("@actions/github");
+const { GitHub, context } = require("@actions/github");
 
 async function run() {
   try {
     // Get token
     const token = core.getInput("github-token", { required: true });
-    // const github = new GitHub(token, {});
+    const github = new GitHub(token, {});
 
-    // Check if the body contains required string
+    const MARKDOWN_IMG_REGEX_PATTERN =
+      /!\[[^\]]*\]\((.*?)\s*("(?:.*[^"])")?\s*\)/;
+    const MARKDOWN_IMG_REGEX = new RegExp(MARKDOWN_IMG_REGEX_PATTERN, "g");
+
+    // Check if the body contains required string from YAML config
     const bodyContains = core.getInput("bodyContains");
+    const checkForImage = core.getBooleanInput("checkForImage");
 
-    if (bodyContains) {
+    if (bodyContains || checkForImage) {
       if (!context.payload.pull_request.body) {
-        core.setFailed("The body of the PR is empty, can't check");
+        core.setFailed(
+          "PRs should have a message or body, please add a description before proceeding to help the other developers."
+        );
       } else {
-        core.info("Latest");
-
         core.info(JSON.stringify(context.payload));
-        if (
-          bodyContains &&
-          context.payload.pull_request.body.indexOf(bodyContains) < 0
-        ) {
-          core.setFailed("The body of the PR does not contain " + bodyContains);
+
+        if (checkForImage && !MARKDOWN_IMG_REGEX.test(bodyContains)) {
+          const pull_request_number = context.payload.pull_request.number;
+
+          const new_comment = github.issues.createComment({
+            ...context.repo,
+            issue_number: pull_request_number,
+            body: "Frontend PRs should include a screenshot for accessibility",
+          });
         }
       }
     }
