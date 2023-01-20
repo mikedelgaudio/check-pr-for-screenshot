e4218f4ce8a62549453eb17461fb091c6da394da
Mike DelGaudio <delgaudiomike@gmail.com>
2023-01-20 10:39:34 -0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # check-pr-for-screenshot
+
+This action checks the body of a PR for a keyword that you've included a screenshot.
+
+## Usage
+
+`bodyContains` allows you to detect if the body of PR has a certain word
+
+```yaml
+name: "Check PR for word"
+on:
+  pull-request:
+    types: [opened, edited, ready_for_review]
+
+jobs:
+  check_pr:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Check PR
+        uses: mikedelgaudio/check-pr-for-screenshot@releases/v0
+        with:
+          github-token: ${{github.token}}
+          bodyContains: "Test"
+```
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import * as core from "@actions/core";
+const { context } = require("@actions/github");
+
+async function run() {
+  try {
+    // Get token
+    const token = core.getInput("github-token", { required: true });
+    // const github = new GitHub(token, {});
+
+    // Check if the body contains required string
+    const bodyContains = core.getInput("bodyContains");
+
+    if (bodyContains) {
+      if (!context.payload.pull_request.body) {
+        core.setFailed("The body of the PR is empty, can't check");
+      } else {
+        if (
+          bodyContains &&
+          context.payload.pull_request.body.indexOf(bodyContains) < 0
+        ) {
+          core.setFailed("The body of the PR does not contain " + bodyContains);
+        }
+      }
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
